4c8d4f6f58f7f4bd892832be0f62c8469a730532
Urinbasarov Dias <jentle.men29@gmail.com>
2018-12-05 18:39:57 +0600

    done

---
+++ OnlineShop(EntFrm)/Models/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineShop_EntFrm_.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ OnlineShop(EntFrm)/Program.cs
@@ -0,0 +1,36 @@
+﻿using OnlineShop_EntFrm_.Data;
+using OnlineShop_EntFrm_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop_EntFrm_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICollection<Item> items = null;
+            using (var context = new ShopContext())
+            {
+                context.Items.Add(new Item
+                {
+                    Name = "tea",
+                    Price = 300
+                });
+
+                context.SaveChanges();
+
+                items = context.Items.ToList();
+            }
+
+            foreach(var item in items)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+        }
+    }
+}
